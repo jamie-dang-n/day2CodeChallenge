a878fdc97126c75ce54843fe221fa5ff6ef1ae29
Anika <pikainka@gmail.com>
2019-07-09 13:14:47 -0700

    First commit

---
+++ Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+//Intention: have a function which determines if a number is odd or even, and outputs a string which tells the result
+//Data Type: Int
+//func oddOrEven (numberNum : Int){
+//    if numberNum % 2 == 0 {
+//        print("\(numberNum) is even")
+//    }
+//    else {
+//        print("\(numberNum) is odd")
+//    }
+//}
+//
+//oddOrEven(numberNum: 10)
+
+//Intention: Find all divisible numbers using a function
+//data types: Int
+
+//func divisNum (inputNum : Double) -> Double {
+//    var array = [1.0,inputNum]
+//    if inputNum % 2.0 == 0.0 {
+//        array.append (2.0, (0.5 * inputNum))
+//        print(array)
+//    }
+//}
+//
+//divisNum(inputNum: 4.0)
+
+//func divisNum (inputNum : Int) -> Int {
+//    if inputNum % < Int
+//}
+
+func divisNum (inputNum : Int) -> Int {
+    if inputNum % 2 == 0 {
+        print(1, inputNum, 2, inputNum/2)
+    } 
+    
+}
+
+divisNum(inputNum: 8)
